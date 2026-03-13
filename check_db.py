89d51258874ef5bd8096a69fa48caa0df7a0f0b4
Reivish Obed Alexander <reivishobed175@gmail.com>
2026-03-13 16:01:32 +0530

    Added automatic database setup script

--- check_db.py
+++ check_db.py
@@ -3,8 +3,7 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM products")
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+print(cursor.fetchall())
 
-rows = cursor.fetchall()
-
-print(rows)
+conn.close()
